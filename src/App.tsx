7c652e7c3fdb58d7561671d28a7f6475a95ca9f6
hanier <hsmp16@gmail.com>
2024-06-02 16:41:01 -0600

    add first file for working with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { BasicTypes } from "./typescript";
+
 function App() {
   return (
     <main>
       <h1>Introduccion a TS - React </h1>
+
+      <BasicTypes />
     </main>
   );
 }
---
+++ src/typescript/BasicTypes.tsx
@@ -0,0 +1,3 @@
+export const BasicTypes = () => {
+  return <div>BasicTypes</div>;
+};
