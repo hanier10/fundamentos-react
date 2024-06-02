8654b79934396359f025163b04974515aa2eabcf
hanier <hsmp16@gmail.com>
2024-06-02 17:41:55 -0600

    viendo tipos basicos

--- src/typescript/BasicTypes.tsx
+++ src/typescript/BasicTypes.tsx
@@ -1,3 +1,15 @@
 export const BasicTypes = () => {
-  return <div>BasicTypes</div>;
+  const name: string = "Hanier";
+  const isActive: boolean = true;
+  const powers = ["NestJS", "Typescript", "React Native", "PostgreSQL"];
+
+  //salto de linea como un enter con br
+  return (
+    <>
+      <h3>Tipos básicos</h3>
+      {name} {isActive ? "true" : "false"}
+      <br />
+      {powers.join(", ")}
+    </>
+  );
 };
